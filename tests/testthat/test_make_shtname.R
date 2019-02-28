a989d606ff1c5836abda13b893bd78abfa85c821
Akira Hayashi <smxshxishxad@me.com>
2019-02-28 22:43:58 +0900

    Add tests

--- tests/testthat/test_make_shtname.R
+++ tests/testthat/test_make_shtname.R
@@ -1,6 +1,26 @@
-context("Make shtname by prefecture")
+context("Make shtname of prefecture")
 
-expect_equal(make_shtname(prefec = "kumamoto", spcs = "katakuchi"), "カタクチ")
-expect_equal(make_shtname(prefec = "kumamoto", spcs = "urume"), "ウルメ")
-expect_equal(make_shtname(prefec = "kumamoto", spcs = "maiwashi"), "マイワシ")
-expect_equal(make_shtname(prefec = "kumamoto", spcs = "sabarui"), "サバ類")
+test_that("make_shtname() makes sheetname for kumamoto data", {
+  expect_equal(make_shtname(prefec = "kumamoto", spcs = "katakuchi"), "カタクチ")
+  expect_equal(make_shtname(prefec = "kumamoto", spcs = "urume"), "ウルメ")
+  expect_equal(make_shtname(prefec = "kumamoto", spcs = "maiwashi"), "マイワシ")
+  expect_equal(make_shtname(prefec = "kumamoto", spcs = "sabarui"), "サバ類")
+  expect_error(make_shtname(prefec = "kumamoto", spcs = "foo"),
+               "Unknown spcs name")
+})
+
+test_that("make_shtname() makes sheetname for nagasaki data", {
+  expect_equal(make_shtname(prefec = "nagasaki", spcs = "katakuchi"), "カタクチ")
+  expect_equal(make_shtname(prefec = "nagasaki", spcs = "urume"), "ウルメ")
+  expect_equal(make_shtname(prefec = "nagasaki", spcs = "maiwashi"), "マイワシ")
+  expect_equal(make_shtname(prefec = "nagasaki", spcs = "masaba"), "マサバ")
+  expect_equal(make_shtname(prefec = "nagasaki", spcs = "gomasaba"), "ゴマサバ")
+  expect_equal(make_shtname(prefec = "nagasaki", spcs = "maaji"), "マアジ")
+  expect_error(make_shtname(prefec = "nagasaki", spcs = "foo"),
+               "Unknown spcs name")
+})
+
+test_that("make_shtname() stops for unknown prefecture", {
+  expect_error(make_shtname(prefec = "foo", spcs = "katakuchi"),
+               "Unknown prefecture")
+})
