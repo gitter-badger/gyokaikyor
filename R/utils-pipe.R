cc1002db09288f0de9d1bbf244905aa1f278bf59
Akira Hayashi <akira.hayashi.1987@gmail.com>
2019-03-05 17:42:41 +0900

    Add double pipe %<>%

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -8,4 +8,11 @@
 #' @export
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
+
+#' @name %<>%
+#' @rdname doublepipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %<>%
+#' @usage lhs \%<>\% rhs
 NULL
